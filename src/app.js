229b1107fc29f568ec216ed28a3e6563662bd417
Frank Trujillo <coachtank@icloud.com>
2024-01-11 04:35:12 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {});
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello! ");
+};
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let subject = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
